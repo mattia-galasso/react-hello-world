b31c17987c5cdba9c654d09684979265a125eeae
MattiaGalasso <galasso.mattia94@gmail.com>
2026-02-03 15:01:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        nisi et corrupti aliquid magni! Rerum nulla aspernatur non harum
+        veritatis maiores necessitatibus earum quos et aut sed inventore, ut
+        doloribus.
+      </p>
+    </>
+  );
+}
+
+export default App;
